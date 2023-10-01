1ea7bc22862e259a9adf4dbbe7bf6808351f5179
ChooSeongWoo <sldoal2423@gmail.com>
2023-10-01 18:23:14 +0900

    로그인 퍼블리싱

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,22 @@ import Header from "../components/Header/Header";
 
 const Login = () => {
   return (
-    <div>
+    <div className="login">
       <Header />
-      로그인이다
+      <div className="container">
+        <h2>로그인</h2>
+        <div className="values">
+          <div className="value">
+            <input type="text" placeholder="아이디" />
+          </div>
+          <div className="value">
+            <input type="text" placeholder="비밀번호" />
+          </div>
+        </div>
+        <div className="sign">
+          <button>로그인</button>
+        </div>
+      </div>
     </div>
   );
 };
