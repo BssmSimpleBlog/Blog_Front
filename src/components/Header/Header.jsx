021b6796dad809bdcc576e141117772b13d6bd4d
ChooSeongWoo <sldoal2423@gmail.com>
2023-10-02 02:50:05 +0900

    ADD :: 토큰 여부에 따른화면

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,28 @@ import "../style.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    window.location.reload();
+  };
   return (
     <div className="header">
       <Link to="/">
         <h2>성우와 승현이의 블로그</h2>
       </Link>
       <div className="auth">
-        <Link to="/auth/login">
-          <h4>로그인</h4>
-        </Link>
-        <Link to="/auth/register">
-          <h4>회원가입</h4>
-        </Link>
+        {!localStorage.getItem("accessToken") ? (
+          <>
+            <Link to="/auth/login">
+              <h4>로그인</h4>
+            </Link>
+            <Link to="/auth/register">
+              <h4>회원가입</h4>
+            </Link>
+          </>
+        ) : (
+          <h4 onClick={logout}>로그아웃</h4>
+        )}
         <h4>프로필</h4>
       </div>
     </div>
