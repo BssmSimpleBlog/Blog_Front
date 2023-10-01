24bff0c01596905eb33b05aab31ce364901e4214
ChooSeongWoo <sldoal2423@gmail.com>
2023-10-02 01:44:33 +0900

    ADD :: 회원가입

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,8 +22,6 @@ const Register = () => {
 
   const [formData, setFormData] = useState(initialFormData);
 
-  console.log({ formData });
-
   const handleSubmit = () =>
     axios
       .post(
@@ -32,14 +30,14 @@ const Register = () => {
         { headers }
       )
       .then(() => {
-        alert("정상적으로 회원가입되었습니다.");
+        alert('정상적으로 회원가입되었습니다.')
         history("/");
       });
 
   return (
     <div className="register">
       <Header />
-      <form className="container">
+      <div className="container">
         <h2>계정 생성</h2>
         <div className="values">
           <div className="value">
@@ -92,12 +90,12 @@ const Register = () => {
           </div>
         </div>
         <div className="sign">
-          <button type="submit" onClick={handleSubmit} className="up">
+          <button onClick={handleSubmit} className="up">
             회원가입
           </button>
           <button>로그인</button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
